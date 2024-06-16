dac4f70d75f6776992f6cb7d1df7d8587e99f530
Lucas Bustos <lbustos.silva@icloud.com>
2024-06-16 12:21:16 -0300

    feat: goals difference HT

--- src/services/BetPlacer.Punter.API/Models/Match/Team/TeamAverageData.cs
+++ src/services/BetPlacer.Punter.API/Models/Match/Team/TeamAverageData.cs
@@ -21,15 +21,43 @@
         public double HomeGoalScoredValueAvg { get; set; }
         public double HomeGoalScoredCostAvg { get; set; }
 
+        public double HomeGoalScoredHTAvg { get; set; }
+        public double HomeGoalScoredHTStd { get; set; }
+        public double HomeGoalScoredHTCv { get; set; }
+        public double HomeGoalScoredValueHTAvg { get; set; }
+        public double HomeGoalScoredCostHTAvg { get; set; }
+
         public double HomeGoalConcededAvg { get; set; }
         public double HomeGoalConcededStd { get; set; }
         public double HomeGoalConcededCv { get; set; }
         public double HomeGoalConcededValueAvg { get; set; }
         public double HomeGoalConcededCostAvg { get; set; }
 
-        public double HomePointsAverage { get; set; }
+        public double HomeGoalConcededHTAvg { get; set; }
+        public double HomeGoalConcededHTStd { get; set; }
+        public double HomeGoalConcededHTCv { get; set; }
+        public double HomeGoalConcededValueHTAvg { get; set; }
+        public double HomeGoalConcededCostHTAvg { get; set; }
+
+        public double HomePointsAvg { get; set; }
         public double HomePointsStd { get; set; }
         public double HomePointsCv { get; set; }
+
+        public double HomePointsHTAvg { get; set; }
+        public double HomePointsHTStd { get; set; }
+        public double HomePointsHTCv { get; set; }
+
+        public double HomeGoalsDifferenceAvg { get; set; }
+        public double HomeGoalsDifferenceStd { get; set; }
+        public double HomeGoalsDifferenceCv { get; set; }
+
+        public double HomeGoalsDifferenceHTAvg { get; set; }
+        public double HomeGoalsDifferenceHTStd { get; set; }
+        public double HomeGoalsDifferenceHTCv { get; set; }
+
+        public double HomeOddsAvg { get; set; }
+        public double HomeOddsStd { get; set; }
+        public double HomeOddsCv { get; set; }
 
         public double AwayGoalScoredAvg { get; set; }
         public double AwayGoalScoredStd { get; set; }
@@ -37,14 +65,42 @@
         public double AwayGoalScoredValueAvg { get; set; }
         public double AwayGoalScoredCostAvg { get; set; }
 
+        public double AwayGoalScoredHTAvg { get; set; }
+        public double AwayGoalScoredHTStd { get; set; }
+        public double AwayGoalScoredHTCv { get; set; }
+        public double AwayGoalScoredValueHTAvg { get; set; }
+        public double AwayGoalScoredCostHTAvg { get; set; }
+
         public double AwayGoalConcededAvg { get; set; }
         public double AwayGoalConcededStd { get; set; }
         public double AwayGoalConcededCv { get; set; }
         public double AwayGoalConcededValueAvg { get; set; }
         public double AwayGoalConcededCostAvg { get; set; }
 
-        public double AwayPointsAverage { get; set; }
+        public double AwayGoalConcededHTAvg { get; set; }
+        public double AwayGoalConcededHTStd { get; set; }
+        public double AwayGoalConcededHTCv { get; set; }
+        public double AwayGoalConcededValueHTAvg { get; set; }
+        public double AwayGoalConcededCostHTAvg { get; set; }
+
+        public double AwayPointsAvg { get; set; }
         public double AwayPointsStd { get; set; }
         public double AwayPointsCv { get; set; }
+
+        public double AwayPointsHTAvg { get; set; }
+        public double AwayPointsHTStd { get; set; }
+        public double AwayPointsHTCv { get; set; }
+
+        public double AwayGoalsDifferenceAvg { get; set; }
+        public double AwayGoalsDifferenceStd { get; set; }
+        public double AwayGoalsDifferenceCv { get; set; }
+
+        public double AwayGoalsDifferenceHTAvg { get; set; }
+        public double AwayGoalsDifferenceHTStd { get; set; }
+        public double AwayGoalsDifferenceHTCv { get; set; }
+
+        public double AwayOddsAvg { get; set; }
+        public double AwayOddsStd { get; set; }
+        public double AwayOddsCv { get; set; }
     }
 }
--- src/services/BetPlacer.Punter.API/Services/CalculateStatsService.cs
+++ src/services/BetPlacer.Punter.API/Services/CalculateStatsService.cs
@@ -52,25 +52,31 @@ namespace BetPlacer.Punter.API.Services
             CalculateScoredAverage(averageData, last10MatchesHome, last10MatchesAway);
             CalculateConcededAverage(averageData, last10MatchesHome, last10MatchesAway);
             CalculatePointsAverage(averageData, last10MatchesHome, last10MatchesAway);
+            CalculateDifferenceGoalsAverage(averageData, last10MatchesHome, last10MatchesAway);
+            CalculateOddsAverage(averageData, last10MatchesHome, last10MatchesAway);
         }
 
         private void CalculateScoredAverage(TeamAverageData averageData, List<MatchBaseData> homeMatches, List<MatchBaseData> awayMatches)
         {
+            double sumHomePercentageOdd = awayMatches.Sum(ma => ma.HomePercentageOdd);
+            double sumAwayPercentageOdd = homeMatches.Sum(mh => mh.AwayPercentageOdd);
+
+            #region Overall
+
             #region Home
 
             double sumHomeScoredGoalsValue = homeMatches.Sum(mh => mh.HomeScoredGoalValue);
             double sumHomeScoredGoalsCost = homeMatches.Sum(mh => mh.HomeScoredGoalCost);
-            double sumAwayPercentageOdd = homeMatches.Sum(mh => mh.AwayPercentageOdd);
 
-            double stdHomeGoalScoredStd = MathUtils.StandardDeviation(homeMatches.Select(mh => (double)mh.HomeGoals).ToList());
+            double stdHomeGoalScored = MathUtils.StandardDeviation(homeMatches.Select(mh => (double)mh.HomeGoals).ToList());
 
             double avgHomeGoalScored = sumHomeScoredGoalsValue / sumAwayPercentageOdd;
             double avgHomeGoalScoredValue = sumHomeScoredGoalsValue / homeMatches.Count();
             double avgHomeGoalScoredCost = sumHomeScoredGoalsCost / homeMatches.Count();
 
             averageData.HomeGoalScoredAvg = Math.Round(avgHomeGoalScored, 2);
-            averageData.HomeGoalScoredStd = Math.Round(stdHomeGoalScoredStd, 2);
-            averageData.HomeGoalScoredCv = Math.Round(stdHomeGoalScoredStd / avgHomeGoalScored, 2);
+            averageData.HomeGoalScoredStd = Math.Round(stdHomeGoalScored, 2);
+            averageData.HomeGoalScoredCv = Math.Round(stdHomeGoalScored / avgHomeGoalScored, 2);
             averageData.HomeGoalScoredValueAvg = Math.Round(avgHomeGoalScoredValue, 2);
             averageData.HomeGoalScoredCostAvg = Math.Round(avgHomeGoalScoredCost, 2);
 
@@ -80,25 +86,70 @@ namespace BetPlacer.Punter.API.Services
 
             double sumAwayScoredGoalsValue = awayMatches.Sum(ma => ma.AwayScoredGoalValue);
             double sumAwayScoredGoalsCost = awayMatches.Sum(ma => ma.AwayScoredGoalCost);
-            double sumHomePercentageOdd = awayMatches.Sum(ma => ma.HomePercentageOdd);
 
-            double stdAwayGoalScoredStd = MathUtils.StandardDeviation(awayMatches.Select(mh => (double)mh.AwayGoals).ToList());
+            double stdAwayGoalScored = MathUtils.StandardDeviation(awayMatches.Select(mh => (double)mh.AwayGoals).ToList());
 
             double avgAwayGoalScored = sumAwayScoredGoalsValue / sumHomePercentageOdd;
             double avgAwayGoalScoredValue = sumAwayScoredGoalsValue / awayMatches.Count();
             double avgAwayGoalScoredCost = sumAwayScoredGoalsCost / awayMatches.Count();
 
             averageData.AwayGoalScoredAvg = Math.Round(avgAwayGoalScored, 2);
-            averageData.AwayGoalScoredStd = Math.Round(stdAwayGoalScoredStd, 2);
-            averageData.AwayGoalScoredCv = Math.Round(stdAwayGoalScoredStd / avgAwayGoalScored, 2);
+            averageData.AwayGoalScoredStd = Math.Round(stdAwayGoalScored, 2);
+            averageData.AwayGoalScoredCv = Math.Round(stdAwayGoalScored / avgAwayGoalScored, 2);
             averageData.AwayGoalScoredValueAvg = Math.Round(avgAwayGoalScoredValue, 2);
             averageData.AwayGoalScoredCostAvg = Math.Round(avgAwayGoalScoredCost, 2);
+
+            #endregion
+
+            #endregion
+
+            #region HT
+
+            #region Home
+
+            double sumHomeScoredGoalsValueHT = homeMatches.Sum(mh => mh.HomeScoredGoalValueHT);
+            double sumHomeScoredGoalsCostHT = homeMatches.Sum(mh => mh.HomeScoredGoalCostHT);
+
+            double stdHomeGoalScoredHT = MathUtils.StandardDeviation(homeMatches.Select(mh => (double)mh.HomeGoalsHT).ToList());
+
+            double avgHomeGoalScoredHT = sumHomeScoredGoalsValueHT / sumAwayPercentageOdd;
+            double avgHomeGoalScoredValueHT = sumHomeScoredGoalsValueHT / homeMatches.Count();
+            double avgHomeGoalScoredCostHT = sumHomeScoredGoalsCostHT / homeMatches.Count();
+
+            averageData.HomeGoalScoredHTAvg = Math.Round(avgHomeGoalScoredHT, 2);
+            averageData.HomeGoalScoredHTStd = Math.Round(stdHomeGoalScoredHT, 2);
+            averageData.HomeGoalScoredHTCv = Math.Round(stdHomeGoalScoredHT / avgHomeGoalScoredHT, 2);
+            averageData.HomeGoalScoredValueHTAvg = Math.Round(avgHomeGoalScoredValueHT, 2);
+            averageData.HomeGoalScoredCostHTAvg = Math.Round(avgHomeGoalScoredCostHT, 2);
+
+            #endregion
+
+            #region Away
+
+            double sumAwayScoredGoalsValueHT = awayMatches.Sum(ma => ma.AwayScoredGoalValueHT);
+            double sumAwayScoredGoalsCostHT = awayMatches.Sum(ma => ma.AwayScoredGoalCostHT);
+
+            double stdAwayGoalScoredHT = MathUtils.StandardDeviation(awayMatches.Select(mh => (double)mh.AwayGoalsHT).ToList());
+
+            double avgAwayGoalScoredHT = sumAwayScoredGoalsValueHT / sumHomePercentageOdd;
+            double avgAwayGoalScoredValueHT = sumAwayScoredGoalsValueHT / awayMatches.Count();
+            double avgAwayGoalScoredCostHT = sumAwayScoredGoalsCostHT / awayMatches.Count();
+
+            averageData.AwayGoalScoredHTAvg = Math.Round(avgAwayGoalScoredHT, 2);
+            averageData.AwayGoalScoredHTStd = Math.Round(stdAwayGoalScoredHT, 2);
+            averageData.AwayGoalScoredHTCv = Math.Round(stdAwayGoalScoredHT / avgAwayGoalScoredHT, 2);
+            averageData.AwayGoalScoredValueHTAvg = Math.Round(avgAwayGoalScoredValueHT, 2);
+            averageData.AwayGoalScoredCostHTAvg = Math.Round(avgAwayGoalScoredCostHT, 2);
+
+            #endregion
 
             #endregion
         }
 
         private void CalculateConcededAverage(TeamAverageData averageData, List<MatchBaseData> homeMatches, List<MatchBaseData> awayMatches)
         {
+            #region Overall
+
             #region Home
 
             double sumHomeConcededGoalsValue = homeMatches.Sum(mh => mh.HomeConcededGoalValue);
@@ -134,20 +185,66 @@ namespace BetPlacer.Punter.API.Services
             averageData.AwayGoalConcededCostAvg = Math.Round(sumAwayConcededGoalsCost / awayMatches.Count(), 2);
 
             #endregion
+
+            #endregion
+
+            #region HT
+
+            #region Home
+
+            double sumHomeConcededGoalsValueHT = homeMatches.Sum(mh => mh.HomeConcededGoalValueHT);
+            double sumHomeConcededGoalsCostHT = homeMatches.Sum(mh => mh.HomeConcededGoalCostHT);
+            double sumAwayPercentageOddHT = homeMatches.Sum(mh => mh.AwayPercentageOdd);
+
+            double stdHomeGoalConcededValueHT = MathUtils.StandardDeviation(homeMatches.Select(mh => (double)mh.AwayGoalsHT).ToList());
+
+            double avgHomeGoalConcededValueHT = sumHomeConcededGoalsValueHT / sumAwayPercentageOdd;
+
+            averageData.HomeGoalConcededHTAvg = Math.Round(avgHomeGoalConcededValueHT, 2);
+            averageData.HomeGoalConcededHTStd = Math.Round(stdHomeGoalConcededValueHT, 2);
+            averageData.HomeGoalConcededHTCv = Math.Round(stdHomeGoalConcededValueHT / avgHomeGoalConcededValueHT, 2);
+            averageData.HomeGoalConcededValueHTAvg = Math.Round(sumHomeConcededGoalsValueHT / homeMatches.Count(), 2);
+            averageData.HomeGoalConcededCostHTAvg = Math.Round(sumHomeConcededGoalsCostHT / homeMatches.Count(), 2);
+
+            #endregion
+
+            #region Away
+
+            double sumAwayConcededGoalsValueHT = awayMatches.Sum(mh => mh.AwayConcededGoalValueHT);
+            double sumAwayConcededGoalsCostHT = awayMatches.Sum(mh => mh.AwayConcededGoalCostHT);
+            double sumHomePercentageOddHT = awayMatches.Sum(mh => mh.HomePercentageOdd);
+
+            double stdAwayGoalConcededValueHT = MathUtils.StandardDeviation(awayMatches.Select(mh => (double)mh.HomeGoalsHT).ToList());
+
+            double avgAwayGoalConcededValueHT = sumAwayConcededGoalsValueHT / sumHomePercentageOdd;
+
+            averageData.AwayGoalConcededHTAvg = Math.Round(avgAwayGoalConcededValueHT, 2);
+            averageData.AwayGoalConcededHTStd = Math.Round(stdAwayGoalConcededValueHT, 2);
+            averageData.AwayGoalConcededHTCv = Math.Round(stdAwayGoalConcededValueHT / avgAwayGoalConcededValueHT, 2);
+            averageData.AwayGoalConcededValueHTAvg = Math.Round(sumAwayConcededGoalsValueHT / awayMatches.Count(), 2);
+            averageData.AwayGoalConcededCostHTAvg = Math.Round(sumAwayConcededGoalsCostHT / awayMatches.Count(), 2);
+
+            #endregion
+
+            #endregion
         }
 
         private void CalculatePointsAverage(TeamAverageData averageData, List<MatchBaseData> homeMatches, List<MatchBaseData> awayMatches)
         {
+            double sumAwayPercentageOdd = homeMatches.Sum(hm => hm.AwayPercentageOdd);
+            double sumHomePercentageOdd = awayMatches.Sum(am => am.HomePercentageOdd);
+            
+            #region Overall
+
             #region Home
 
             double sumHomePointsValue = homeMatches.Sum(hm => hm.HomePointsValue);
-            double sumAwayPercentageOdd = homeMatches.Sum(hm => hm.AwayPercentageOdd);
 
             double stdHomePoints = MathUtils.StandardDeviation(homeMatches.Select(hm => (double)hm.HomePoints).ToList());
 
             double avgHomePointsValue = sumHomePointsValue / sumAwayPercentageOdd;
 
-            averageData.HomePointsAverage = Math.Round(avgHomePointsValue, 2);
+            averageData.HomePointsAvg = Math.Round(avgHomePointsValue, 2);
             averageData.HomePointsStd = Math.Round(stdHomePoints, 2);
             averageData.HomePointsCv = Math.Round(stdHomePoints / avgHomePointsValue, 2);
 
@@ -156,15 +253,149 @@ namespace BetPlacer.Punter.API.Services
             #region Away
 
             double sumAwayPointsValue = awayMatches.Sum(am => am.AwayPointsValue);
-            double sumHomePercentageOdd = awayMatches.Sum(am => am.HomePercentageOdd);
 
             double stdAwayPoints = MathUtils.StandardDeviation(awayMatches.Select(am => (double)am.AwayPoints).ToList());
 
             double avgAwayPointsValue = sumAwayPointsValue / sumHomePercentageOdd;
 
-            averageData.AwayPointsAverage = Math.Round(avgAwayPointsValue, 2);
+            averageData.AwayPointsAvg = Math.Round(avgAwayPointsValue, 2);
             averageData.AwayPointsStd = Math.Round(stdAwayPoints, 2);
             averageData.AwayPointsCv = Math.Round(stdAwayPoints / avgAwayPointsValue, 2);
+
+            #endregion
+
+            #endregion
+
+            #region HT
+
+            #region Home
+
+            double sumHomePointsValueHT = homeMatches.Sum(hm => hm.HomePointsValueHT);
+
+            double stdHomePointsHT = MathUtils.StandardDeviation(homeMatches.Select(hm => (double)hm.HomePointsHT).ToList());
+
+            double avgHomePointsValueHT = sumHomePointsValueHT / sumAwayPercentageOdd;
+
+            averageData.HomePointsHTAvg = Math.Round(avgHomePointsValueHT, 2);
+            averageData.HomePointsHTStd = Math.Round(stdHomePointsHT, 2);
+            averageData.HomePointsHTCv = Math.Round(stdHomePointsHT / avgHomePointsValueHT, 2);
+
+            #endregion
+
+            #region Away
+
+            double sumAwayPointsValueHT = awayMatches.Sum(am => am.AwayPointsValueHT);
+
+            double stdAwayPointsHT = MathUtils.StandardDeviation(awayMatches.Select(am => (double)am.AwayPointsHT).ToList());
+
+            double avgAwayPointsValueHT = sumAwayPointsValueHT / sumHomePercentageOdd;
+
+            averageData.AwayPointsHTAvg = Math.Round(avgAwayPointsValueHT, 2);
+            averageData.AwayPointsHTStd = Math.Round(stdAwayPointsHT, 2);
+            averageData.AwayPointsHTCv = Math.Round(stdAwayPointsHT / avgAwayPointsValueHT, 2);
+
+            #endregion
+
+            #endregion
+        }
+
+        private void CalculateDifferenceGoalsAverage(TeamAverageData averageData, List<MatchBaseData> homeMatches, List<MatchBaseData> awayMatches)
+        {
+            double sumAwayPercentageOdd = homeMatches.Sum(mh => mh.AwayPercentageOdd);
+            double sumHomePercentageOdd = awayMatches.Sum(mh => mh.HomePercentageOdd);
+
+            #region Overall
+
+            #region Home
+
+            double sumHomeGoalsDifferenceValue = homeMatches.Sum(mh => mh.HomeGoalsDifferenceValue);
+
+            double stdHomeGoalsDifferenceValue = MathUtils.StandardDeviation(homeMatches.Select(mh => (double)mh.HomeGoalsDifference).ToList());
+
+            double avgHomeGoalDifferenceValue = sumHomeGoalsDifferenceValue / sumAwayPercentageOdd;
+
+            averageData.HomeGoalsDifferenceAvg = Math.Round(avgHomeGoalDifferenceValue, 2);
+            averageData.HomeGoalsDifferenceStd = Math.Round(stdHomeGoalsDifferenceValue, 2);
+            averageData.HomeGoalsDifferenceCv = Math.Abs(Math.Round(stdHomeGoalsDifferenceValue / avgHomeGoalDifferenceValue, 2));
+
+            #endregion
+
+            #region Away
+
+            double sumAwayGoalsDifferenceValue = awayMatches.Sum(mh => mh.AwayGoalsDifferenceValue);
+
+            double stdAwayGoalsDifferenceValue = MathUtils.StandardDeviation(awayMatches.Select(mh => (double)mh.AwayGoalsDifference).ToList());
+
+            double avgAwayGoalDifferenceValue = sumAwayGoalsDifferenceValue / sumHomePercentageOdd;
+
+            averageData.AwayGoalsDifferenceAvg = Math.Round(avgAwayGoalDifferenceValue, 2);
+            averageData.AwayGoalsDifferenceStd = Math.Round(stdAwayGoalsDifferenceValue, 2);
+            averageData.AwayGoalsDifferenceCv = Math.Abs(Math.Round(stdAwayGoalsDifferenceValue / avgAwayGoalDifferenceValue, 2));
+
+            #endregion
+
+            #endregion
+
+            #region HT
+
+            #region Home
+
+            double sumHomeGoalsDifferenceValueHT = homeMatches.Sum(mh => mh.HomeGoalsDifferenceValueHT);
+
+            double stdHomeGoalsDifferenceValueHT = MathUtils.StandardDeviation(homeMatches.Select(mh => (double)mh.HomeGoalsDifferenceHT).ToList());
+
+            double avgHomeGoalDifferenceValueHT = sumHomeGoalsDifferenceValueHT / sumAwayPercentageOdd;
+
+            averageData.HomeGoalsDifferenceHTAvg = Math.Round(avgHomeGoalDifferenceValueHT, 2);
+            averageData.HomeGoalsDifferenceHTStd = Math.Round(stdHomeGoalsDifferenceValueHT, 2);
+            averageData.HomeGoalsDifferenceHTCv = Math.Abs(Math.Round(stdHomeGoalsDifferenceValueHT / avgHomeGoalDifferenceValueHT, 2));
+
+            #endregion
+
+            #region Away
+
+            double sumAwayGoalsDifferenceValueHT = awayMatches.Sum(mh => mh.AwayGoalsDifferenceValueHT);
+
+            double stdAwayGoalsDifferenceValueHT = MathUtils.StandardDeviation(awayMatches.Select(mh => (double)mh.AwayGoalsDifferenceHT).ToList());
+
+            double avgAwayGoalDifferenceValueHT = sumAwayGoalsDifferenceValueHT / sumHomePercentageOdd;
+
+            averageData.AwayGoalsDifferenceHTAvg = Math.Round(avgAwayGoalDifferenceValueHT, 2);
+            averageData.AwayGoalsDifferenceHTStd = Math.Round(stdAwayGoalsDifferenceValueHT, 2);
+            averageData.AwayGoalsDifferenceHTCv = Math.Abs(Math.Round(stdAwayGoalsDifferenceValueHT / avgAwayGoalDifferenceValueHT, 2));
+
+            #endregion
+
+            #endregion
+        }
+
+        private void CalculateOddsAverage(TeamAverageData averageData, List<MatchBaseData> homeMatches, List<MatchBaseData> awayMatches)
+        {
+            #region Home
+
+            var sumHomeOdds = homeMatches.Sum(hm => hm.HomeOdd);
+
+            var stdHomeOdds = MathUtils.StandardDeviation(homeMatches.Select(hm => hm.HomeOdd).ToList());
+
+            var avgHomeOdds = sumHomeOdds / homeMatches.Count();
+
+            averageData.HomeOddsAvg = Math.Round(avgHomeOdds, 2);
+            averageData.HomeOddsStd = Math.Round(stdHomeOdds, 2);
+            averageData.HomeOddsCv = Math.Round(stdHomeOdds / avgHomeOdds, 2);
+
+            #endregion
+
+            #region Away
+
+            var sumAwayOdds = awayMatches.Sum(hm => hm.AwayOdd);
+
+            var stdAwayOdds = MathUtils.StandardDeviation(awayMatches.Select(hm => hm.AwayOdd).ToList());
+
+            var avgAwayOdds = sumAwayOdds / awayMatches.Count();
+
+            averageData.AwayOddsAvg = Math.Round(avgAwayOdds, 2);
+            averageData.AwayOddsStd = Math.Round(stdAwayOdds, 2);
+            averageData.AwayOddsCv = Math.Round(stdAwayOdds / avgAwayOdds, 2);
 
             #endregion
         }
